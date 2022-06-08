263868944c870c8171b3e689d84e1b74c4a2e2a1
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-06-08 18:11:00 +0100

    fix: screen size mixin injection

--- graphery-v2/src/components/mixins/screen-sizes.ts
+++ graphery-v2/src/components/mixins/screen-sizes.ts
@@ -1,26 +1,49 @@
-import { computed, reactive } from 'vue';
+import { provide, inject, computed, reactive } from 'vue';
+import type { UnwrapNestedRefs, ComputedRef } from 'vue';
 import { useQuasar } from 'quasar';
 
-const { screen } = useQuasar();
+export interface ScreenSizes {
+    smallAndLarger: ComputedRef<boolean>;
+    mediumAndLarger: ComputedRef<boolean>;
+    largeAndLarger: ComputedRef<boolean>;
+    smallAndLower: ComputedRef<boolean>;
+    mediumAndLower: ComputedRef<boolean>;
+    largeAndLower: ComputedRef<boolean>;
+}
 
-const screenSizes = reactive({
-    // enlarge
-    smallAndLarger: computed(() => screen.gt.xs),
-    mediumAndLarger: computed(() => screen.gt.sm),
-    largeAndLarger: computed(() => screen.gt.md),
-    // shrink
-    smallAndLower: computed(() => screen.lt.md),
-    mediumAndLower: computed(() => screen.lt.lg),
-    largeAndLower: computed(() => screen.lt.xl),
-});
+const _SCREEN_SIZES_KEY = '__ScreenSizes';
 
 /**
- * returns a collection of readable screen sizes
+ * Constructs a collection of readable screen sizes
  * This application has three split points, small, medium, and large
  * The following values can be used instead of q.screen
  *
+ * This function uses provide/inject and is intended to be used
+ * in the root component of the application.
+ *
+ * `useScreenSizes` is the injection pair
+ *
  * @returns the size collection
  */
-export function useScreenSizes() {
-    return screenSizes;
+export function getScreenSizes(): UnwrapNestedRefs<ScreenSizes> {
+    const screen = useQuasar().screen;
+
+    const sizes = reactive({
+        // enlarge
+        smallAndLarger: computed(() => screen.gt.xs),
+        mediumAndLarger: computed(() => screen.gt.sm),
+        largeAndLarger: computed(() => screen.gt.md),
+        // shrink
+        smallAndLower: computed(() => screen.lt.md),
+        mediumAndLower: computed(() => screen.lt.lg),
+        largeAndLower: computed(() => screen.lt.xl),
+    });
+
+    provide(_SCREEN_SIZES_KEY, sizes);
+
+    return sizes;
+}
+
+export function useScreenSizes(): ScreenSizes {
+    return inject(_SCREEN_SIZES_KEY) as ScreenSizes;
 }
