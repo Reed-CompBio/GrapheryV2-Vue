035f8f041fa988725a87b64505b4497be9f0c570
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-06-12 09:51:56 -0700

    feat: added License Card

---
+++ graphery-v2/src/components/general/LicenseCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-card class="q-px-md q-py-lg q-mt-xl q-mb-sm license-card white-a">
+        <p>
+            {{ t('license.licenseInfo1') }}
+            <a
+                rel="license"
+                target="_blank"
+                href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            >
+                {{ t('license.licenseInfo2') }}
+            </a>
+        </p>
+        <p class="no_mb_p">
+            <a
+                rel="license"
+                target="_blank"
+                href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            >
+                <img
+                    alt="Creative Commons License"
+                    style="border-width: 0"
+                    src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"
+                />
+            </a>
+        </p>
+    </q-card>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
+
+<style lang="sass">
+.no_mb_p
+    margin-bottom: 0
+.license-card
+    background-color: #a70e16
+    color: white
+    a
+        color: inherit
+        text-decoration: underline
+</style>
