5308fd1423edb2184a550519ab006b0f7c81b98d
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-06-29 14:08:25 -0700

    feat: layout variable display

---
+++ graphery-v2/src/components/workspace/code-area/VariableCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-card> </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { CompositionalObjectIdentityType } from 'src/types/execution-types';
+import type { PropType } from 'vue';
+
+export default defineComponent({
+    props: {
+        variable: {
+            type: Object as PropType<CompositionalObjectIdentityType>,
+            default: null,
+        },
+    },
+    setup() {
+        return {};
+    },
+});
+</script>
