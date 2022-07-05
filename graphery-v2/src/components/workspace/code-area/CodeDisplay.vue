0b0948d186c9cc41a23ac4a5f4914f504d5ef247
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-07-04 17:56:28 -0700

    feat: add code display

---
+++ graphery-v2/src/components/workspace/code-area/CodeDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+    <FullHeightSplitter v-model="pos">
+        <template v-slot:before>
+            <MonacoEditor />
+        </template>
+        <template v-slot:after>
+            <VariableDisplay />
+        </template>
+    </FullHeightSplitter>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import FullHeightSplitter from 'components/workspace/frames/FullHeightSplitter.vue';
+import MonacoEditor from 'components/workspace/code-area/MonacoEditor.vue';
+import VariableDisplay from 'components/workspace/code-area/variable-area/VariableDisplay.vue';
+
+export default defineComponent({
+    components: { VariableDisplay, MonacoEditor, FullHeightSplitter },
+    setup() {
+        const pos = ref(0.8);
+        return {
+            pos,
+        };
+    },
+});
+</script>
