7fb4195f36152903fe673e52619ea3fa5ddb8ed1
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-07-14 09:58:53 -0700

    fix: variable card display

--- graphery-v2/src/components/workspace/code-area/variable-area/VariableCard.vue
+++ graphery-v2/src/components/workspace/code-area/variable-area/VariableCard.vue
@@ -1,26 +1,43 @@
 <template>
     <q-card>
-        <VariableCardHeader :info="info" />
+        <VariableCardHeader :info="wrappedInfo" />
         <q-separator />
-        <VariableBody :info="info" />
+        <VariableBody :info="wrappedInfo" />
     </q-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
 import VariableCardHeader from 'components/workspace/code-area/variable-area/VariableCardHeader.vue';
 import VariableBody from 'components/workspace/code-area/variable-area/VariableBody.vue';
 
 import type { PropType } from 'vue';
-import type { VariableInfo } from 'components/mixins/variable-base';
+import { VariableInfoWrapper } from 'components/mixins/variable-base';
+import { CompositionalObjectIdentityType } from 'src/types/execution-types';
 
 export default defineComponent({
     components: { VariableBody, VariableCardHeader },
     props: {
         info: {
-            type: Object as PropType<VariableInfo>,
+            type: Object as PropType<
+                CompositionalObjectIdentityType & { label: string }
+            >,
             required: true,
         },
+    },
+    setup(props) {
+        const wrappedInfo = ref(
+            new VariableInfoWrapper(props.info, props.info.label)
+        );
+
+        watchEffect(() => {
+            wrappedInfo.value.updateBase(props.info);
+            wrappedInfo.value.requestHighlight();
+        });
+
+        return {
+            wrappedInfo,
+        };
     },
 });
 </script>
--- graphery-v2/src/components/workspace/code-area/variable-area/VariableDisplay.vue
+++ graphery-v2/src/components/workspace/code-area/variable-area/VariableDisplay.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="var-display-wrapper">
-        <div
-            v-for="(variable, index) in variables"
-            :key="index"
-            class="var-display-variable"
-        >
-            <VariableCard :info="variable" />
+        <div class="var-display-accessed-variables">
+            <div
+                v-for="(variable, index) in accessedVariables"
+                :key="index"
+                class="var-display-variable"
+            >
+                <VariableCard :info="variable" />
+            </div>
+        </div>
+        <div class="var-display-variables">
+            <div
+                v-for="variable in variables"
+                :key="variable.label"
+                class="var-display-variable"
+            >
+                <VariableCard :info="variable" />
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive, watch } from 'vue';
+import { computed, defineComponent, watch } from 'vue';
 import { useHeadquarterStorage } from 'stores/headquarter-storage';
 import { storeToRefs } from 'pinia';
-import { VariableInfoWrapper } from 'components/mixins/variable-base';
 import VariableCard from 'components/workspace/code-area/variable-area/VariableCard.vue';
 import { useGraphBus } from 'src/components/mixins/controller/graph-bus';
 
@@ -22,67 +32,30 @@ export default defineComponent({
     components: { VariableCard },
     setup() {
         const storage = useHeadquarterStorage();
-        // const refresh = storage.refreshStepRecord;
         const { currentStepRecord } = storeToRefs(storage);
-        const fixedVariables = reactive<Record<string, VariableInfoWrapper>>(
-            Object.fromEntries(
-                Object.entries(currentStepRecord.value?.variables ?? {}).map(
-                    ([key, value]) => {
-                        return [key, new VariableInfoWrapper(value, key)];
-                    }
-                )
+
+        const fixedNamedVars = computed(() =>
+            Object.entries(currentStepRecord.value?.variables ?? {}).map(
+                ([key, value]) => {
+                    return {
+                        label: key,
+                        ...value,
+                    };
+                }
             )
         );
-        const accessedVariables = computed<VariableInfoWrapper[]>(() =>
+        const accessedVariables = computed(() =>
             (currentStepRecord.value?.accesses ?? []).map((access) => {
-                return new VariableInfoWrapper(access, 'returned variable');
+                return { label: 'returned variable', ...access };
             })
         );
-        const _variables = computed<VariableInfoWrapper[]>(() => {
-            return [
-                ...accessedVariables.value,
-                ...Object.values(fixedVariables),
-            ];
-        });
+
         const graphBus = useGraphBus();
 
-        watch(
-            () => currentStepRecord.value?.variables,
-            (newVal) => {
-                if (newVal === undefined) {
-                    for (const prop of Object.getOwnPropertyNames(
-                        fixedVariables
-                    )) {
-                        delete fixedVariables[prop];
-                    }
-                } else {
-                    for (const key of Object.getOwnPropertyNames(newVal)) {
-                        if (!(key in fixedVariables)) {
-                            fixedVariables[key] = new VariableInfoWrapper(
-                                newVal[key],
-                                key
-                            );
-                        } else {
-                            if (
-                                newVal[key].pythonId !==
-                                fixedVariables[key].variable.pythonId
-                            ) {
-                                fixedVariables[key].updateBase(newVal[key]);
-                            }
-                        }
-                    }
-                }
-            }
-        );
-
-        watch(_variables, (newVal) => {
+        watch(fixedNamedVars, () => {
             graphBus.emit('clear-highlight');
-
-            for (const variable of newVal) {
-                variable.requestHighlight();
-            }
         });
-        return { variables: _variables };
+        return { variables: fixedNamedVars, accessedVariables };
     },
 });
 </script>
